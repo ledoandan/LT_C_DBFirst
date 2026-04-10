a841c0ff5bda5fa39b5a39384553aa780631ff4f
LE DOAN DAN <ledoandan2110@gmail.com>
2026-04-11 01:00:37 +0700

    Scaffold tu database, tao Models và DbContext

---
+++ DBFirst/DBFirst/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace DBFirst.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
--- DBFirst/DBFirst/obj/Debug/net10.0/DBFirst.AssemblyInfo.cs
+++ DBFirst/DBFirst/obj/Debug/net10.0/DBFirst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dacd31cde37d481cd862281dfb16b11d0b81a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3120dfbd7ba5a8923f02d17a96dded2906f60814")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
